ac7f56191568e7cfca13ca4ec371034c526d6026
Jay Jonicx <havugimana.jonathan4@gmail.com>
2021-04-03 11:54:28 +0200

    Finished_Login_UI

---
+++ ikimina_cyacu_frontend/src/frontface/pages/client/auth/login.js
@@ -0,0 +1,83 @@
+import {
+	Container,
+	Form,
+	Row,
+	Col,
+	Button,
+	Image,
+} from 'react-bootstrap';
+import './index.css';
+import LoginImage from '../../../../assets/login.png';
+
+export const LoginView = () => {
+	return (
+			<section className='auth-form py-5'>
+				<Container>
+					<Row>
+						<Col lg={5}>
+							<Form
+								className='p-5 shadow mt-5 border'
+							>
+								<Row>
+									<Col lg={12}>
+										<h4 className='text-bold' style={{ color: '#000' }}>
+											Login
+										</h4>
+
+										<p className='text-capitalize text-bold'>
+											Welcome Back Admin, Happy Registration
+										</p>
+									</Col>
+								</Row>
+
+								<Form.Group>
+									<Form.Control
+										type='text'
+										name='username'
+										placeholder='User Name'
+										required={true}
+										autoFocus={true}
+										autoComplete={false}
+									/>
+
+									<Form.Control.Feedback type='invalid'>
+										Email address is required
+									</Form.Control.Feedback>
+								</Form.Group>
+
+								<Form.Group>
+									<Form.Control
+										type='password'
+										name='password'
+										placeholder='Password'
+										required={true}
+										autoComplete={false}
+									/>
+
+									<Form.Control.Feedback type='invalid'>
+										Password is required
+									</Form.Control.Feedback>
+
+								</Form.Group>
+
+								<Button
+									type='submit'
+									variant='primary'
+									className='btn-block mt-4 py-3'
+									style={{ fontSize: '12px' }}
+								>
+									<>
+										<i className='fa fa-lock mr-1'></i> Sign In
+									</>
+								</Button>
+							</Form>
+						</Col>
+
+						<Col lg={7}>
+							<Image src={LoginImage} alt='Login' className='img-fluid' />
+						</Col>
+					</Row>
+				</Container>
+			</section>
+	);
+};
