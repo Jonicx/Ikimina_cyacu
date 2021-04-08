c59dc17c6ad56c90a0e31018d6708a034c36a058
Pierratono Costa <pierratonoc@gmail.com>
2021-04-08 02:12:13 +0200

    Get All Members

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, abort, reqparse
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from sqlalchemy.sql import func
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,6 +16,7 @@ api = Api(app)
 app.config['SECRET_KEY'] = '!9m@S-dThyIlW[pHQbN^'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/ujamaDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ma = Marshmallow(app)
 hash_id = Hashids(alphabet='0123456789ABCDEF', salt='IKIMINA', min_length=6)
 db = SQLAlchemy(app)
 
@@ -46,6 +48,15 @@ class MemberModel(db.Model):
     level = db.Column(db.Integer, nullable=False, default=0)
     createdAt = db.Column(db.DateTime(), server_default=func.now())
     updatedAt = db.Column(db.DateTime(), onupdate=func.now())
+
+
+class MemberSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'memberId', 'firstName', 'lastName', 'phoneNumber', 'parentMemberId', 'members', 'level')
+
+
+memberSchema = MemberSchema()
+membersSchema = MemberSchema(many=True)
 
 
 class LogsModel(db.Model):
@@ -168,11 +179,11 @@ class RegisterMember(Resource):
                 db.session.add(recordLog)
                 db.session.commit()
 
-                if not args['parentMemberId']==0:
+                if not args['parentMemberId'] == 0:
                     setattr(parentMember, 'members', parentMember.members + 1)
                     db.session.commit()
 
-                return {"message": "Member has been Added successfully","memberID":newMember.memberId}, 200
+                return {"message": "Member has been Added successfully", "member": memberSchema.dump(newMember)}, 200
 
             else:
                 return {"message": f"Orientation with ID: {parentMember.memberId}, already has two members!"}
@@ -182,9 +193,34 @@ class RegisterMember(Resource):
             return {"message": f"Member registration failed!"}, 400
 
 
+class GetMembers(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('token', location='headers', required=True, help="Token cannot be blank!")
+        args = parser.parse_args()
+
+        decodedJWT = jwt.decode(args['token'], "secret", algorithms=["HS256"])
+        exists = AdminModel.query.filter_by(username=decodedJWT['username']).first()
+        if not exists:
+            return {"message": "Token invalid"}
+        now = datetime.datetime.now().timestamp()
+        if not now < decodedJWT['exp']:
+            return {"message": "Token expired"}
+        try:
+            allMembers = MemberModel.query.all()
+            result = membersSchema.dump(allMembers)
+
+            return result, 200
+
+        except IntegrityError:
+            db.session.rollback()
+            return {"message": f"Member registration failed!"}, 400
+
+
 api.add_resource(RegisterAdmin, "/auth/admin/register")
 api.add_resource(LoginAdmin, "/auth/admin/login")
 api.add_resource(RegisterMember, "/member/add")
+api.add_resource(GetMembers, "/member/get")
 
 # Init Server
 
