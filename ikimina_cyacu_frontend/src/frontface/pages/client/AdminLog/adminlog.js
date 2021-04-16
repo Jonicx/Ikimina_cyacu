b337630951dc6e785a5f70fc81b02471d7c89819
Pierratono Costa <pierratonoc@gmail.com>
2021-04-16 08:14:06 +0200

    logs-data-table

--- ikimina_cyacu_frontend/src/frontface/pages/client/AdminLog/adminlog.js
+++ ikimina_cyacu_frontend/src/frontface/pages/client/AdminLog/adminlog.js
@@ -1,10 +1,31 @@
+import { useState, useEffect } from "react";
+
 import { Container, Row, Col } from "react-bootstrap";
 import "./index.css";
 import AppLayout from "../../../layouts/AppLayout";
-import { SlidebarPages } from '../../../components/SidebarPages';
+import { SlidebarPages } from "../../../components/SidebarPages";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 
+import LogService from "../../../../service/logs.service";
+
 export const AdminLogView = () => {
+  const [rawLogs, setRawLogs] = useState([]);
+
+  useEffect(() => {
+    loadLogs();
+  }, []);
+
+  const loadLogs = () => {
+    LogService.getAllLogs().then((res) => {
+      let rawData = res.data;
+      const logs = rawData.map((l) => ({
+        ...l,
+        adminId: "Admin",
+      }));
+      console.log(logs);
+      setRawLogs(logs);
+    });
+  };
 
   return (
     <AppLayout>
@@ -23,16 +44,14 @@ export const AdminLogView = () => {
 
               <Col>
                 <div>
-                  <BootstrapTable pagination>
-                    <TableHeaderColumn dataField="Column_Number" isKey>
+                  <BootstrapTable pagination data={rawLogs} striped hover>
+                    <TableHeaderColumn dataField="id" isKey>
                       #
                     </TableHeaderColumn>
-                    <TableHeaderColumn dataField="Username">Username</TableHeaderColumn>
-                    <TableHeaderColumn dataField="Action">Action</TableHeaderColumn>
-                    <TableHeaderColumn dataField="Description">
-                      Description
-                    </TableHeaderColumn>
-                    <TableHeaderColumn dataField="Time_stamp">
+                    <TableHeaderColumn dataField="adminId">Username</TableHeaderColumn>
+                    <TableHeaderColumn dataField="activity">Activity</TableHeaderColumn>
+                    <TableHeaderColumn dataField="details">Description</TableHeaderColumn>
+                    <TableHeaderColumn dataField="timestamp" dataSort={true}>
                       Timestamp
                     </TableHeaderColumn>
                   </BootstrapTable>
