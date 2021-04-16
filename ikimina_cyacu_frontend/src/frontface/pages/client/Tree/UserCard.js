5552c55f3ef3d2ac0ff76e1c6bd7f81bc56cb060
Pierratono Costa <pierratonoc@gmail.com>
2021-04-16 08:25:26 +0200

    feat-orientation-modal

--- ikimina_cyacu_frontend/src/frontface/pages/client/Tree/UserCard.js
+++ ikimina_cyacu_frontend/src/frontface/pages/client/Tree/UserCard.js
@@ -11,30 +11,33 @@ export default class UserCard extends React.PureComponent {
         <Col xs={9}>
           <ListGroup variant="flush">
             <ListGroup.Item variant="primary">
-              <strong>ID:</strong> {this.props.userData.memberId}
+              <strong>ID:</strong> {this.props.userData.member.memberId}
             </ListGroup.Item>
             <ListGroup.Item>
-              <strong>Names:</strong> {this.props.userData.firstName}{" "}
+              <strong>Names:</strong> {this.props.userData.member.firstName}{" "}
               {this.props.userData.lastName}
             </ListGroup.Item>
             <ListGroup.Item>
-              <strong>Telephone:</strong> +25{this.props.userData.phoneNumber}
+              <strong>Telephone:</strong> +25{this.props.userData.member.phoneNumber}
             </ListGroup.Item>
             <ListGroup.Item>
               <strong>Registered:</strong>
-              {`${moment(this.props.userData.createdAt).format(" Do MMMM YYYY hh:mm A")}`}
+              {`${moment(this.props.userData.member.createdAt).format(
+                " Do MMMM YYYY hh:mm A"
+              )}`}
             </ListGroup.Item>
             <ListGroup.Item variant="dark">
               <h4>Oriented to:</h4>
               <ListGroup variant="flush">
                 <ListGroup.Item variant="dark">
-                  Orientation ID: {this.props.userData.memberId}
+                  Orientation ID: {this.props.userData.orientation.memberId}
                 </ListGroup.Item>
                 <ListGroup.Item variant="dark">
-                  Names: {this.props.userData.firstName} {this.props.userData.lastName}
+                  Names: {this.props.userData.orientation.firstName}{" "}
+                  {this.props.userData.orientation.lastName}
                 </ListGroup.Item>
                 <ListGroup.Item variant="dark">
-                  Telephone: +25{this.props.userData.phoneNumber}
+                  Telephone: +25{this.props.userData.orientation.phoneNumber}
                 </ListGroup.Item>
               </ListGroup>
             </ListGroup.Item>
