d84b05e02f3b86d66f0bc3d602f67c52ae32a78a
Pierratono Costa <pierratonoc@gmail.com>
2021-04-17 15:13:29 +0200

    rebase-costa-main

--- ikimina_cyacu_frontend/src/frontface/pages/client/Tree/MemberNode.js
+++ ikimina_cyacu_frontend/src/frontface/pages/client/Tree/MemberNode.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Modal, Col, Row } from "react-bootstrap";
 import PropTypes from "prop-types";
 import MemberService from "../../../../service/members.service";
-import dropdown from "../../../../assets/drop_down_16px.png"
+import dropdown from "../../../../assets/drop_down_16px.png";
 
 import "./index.css";
 const moment = require("moment");
@@ -16,24 +16,24 @@ const MemberNode = ({ nodeData }) => {
 
   const handleClose = () => setShow(false);
   const handleShow = (id) => {
-    // MemberService.getMemberById(id).then((res) => {
-    //   console.log(res.data);
-    // });
-    console.log(id);
+    MemberService.getMemberById(id).then((res) => {
+      console.log(res.data);
+    });
+    // console.log(id);
     setShow(true);
   };
   return (
     <>
       {nodeData ? (
-        <div onClick={handleShow}>
+        <div>
           <div className={nodeData.level === 0 ? "red" : "green"}>{}</div>
           <Col className="white">
             <Row className="mb-1">
               <Col>
-              <strong id="code"> {nodeData.memberId}</strong>
+                <strong id="code"> {nodeData.memberId}</strong>
               </Col>
               <Col>
-              <img src={dropdown} alt=""/>
+                <img src={dropdown} onClick={() => handleShow(nodeData.id)} alt="" />
               </Col>
             </Row>
             <p>
