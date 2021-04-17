1f99afe578f8c7d7541af5fa14388114b526f6f8
Pierratono Costa <pierratonoc@gmail.com>
2021-04-17 15:33:52 +0200

    fix-search

--- ikimina_cyacu_frontend/src/frontface/pages/client/Tree/MemberNode.js
+++ ikimina_cyacu_frontend/src/frontface/pages/client/Tree/MemberNode.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Modal, Col, Row } from "react-bootstrap";
+import { Modal, Col, Row, ListGroup } from "react-bootstrap";
 import PropTypes from "prop-types";
 import MemberService from "../../../../service/members.service";
 import dropdown from "../../../../assets/drop_down_16px.png";
+import ID from "../../../../assets/id.svg";
 
 import "./index.css";
 const moment = require("moment");
@@ -13,13 +14,13 @@ const propTypes = {
 
 const MemberNode = ({ nodeData }) => {
   const [show, setShow] = useState(false);
+  const [currentMember, setCurrentMember] = useState({});
 
   const handleClose = () => setShow(false);
   const handleShow = (id) => {
     MemberService.getMemberById(id).then((res) => {
-      console.log(res.data);
+      setCurrentMember(res.data);
     });
-    // console.log(id);
     setShow(true);
   };
   return (
@@ -57,7 +58,47 @@ const MemberNode = ({ nodeData }) => {
       )}
       <>
         <Modal show={show} onHide={handleClose}>
-          <h1>{nodeData.id}</h1>
+          {/* <h1>{nodeData.id}</h1> */}
+          <Row>
+            <Col xs={9}>
+              <ListGroup variant="flush">
+                <ListGroup.Item variant="primary">
+                  {/* <strong>ID:</strong> {currentMember.member.memberId} */}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {/* <strong>Names:</strong> {currentMember.member.firstName}{" "} */}
+                  {/* {currentMember.member.lastName} */}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {/* <strong>Telephone:</strong> +25{currentMember.member.phoneNumber} */}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <strong>Registered:</strong>
+                  {/* {`${moment(currentMember.member.createdAt).format(
+                    " Do MMMM YYYY hh:mm A"
+                  )}`} */}
+                </ListGroup.Item>
+                <ListGroup.Item variant="dark">
+                  <h4>Oriented to:</h4>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item variant="dark">
+                      {/* Orientation ID: {currentMember.orientation.memberId} */}
+                    </ListGroup.Item>
+                    <ListGroup.Item variant="dark">
+                      {/* Names: {currentMember.orientation.firstName}{" "}
+                      {currentMember.orientation.lastName} */}
+                    </ListGroup.Item>
+                    <ListGroup.Item variant="dark">
+                      {/* Telephone: +25{currentMember.orientation.phoneNumber} */}
+                    </ListGroup.Item>
+                  </ListGroup>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col xs={3}>
+              <img src={ID} alt="icon" className="id_card align-self-center my-auto" />
+            </Col>
+          </Row>
         </Modal>
       </>
     </>
